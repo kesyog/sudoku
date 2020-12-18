02cf4fd54bd68d1cd35255d490bc0fe240342d4a
Kesavan Yogeswaran <kesyog@gmail.com>
2020-12-18 14:29:58 -0500

    Add rustdoc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! # Sudoku
+//!
+//! A Sudoku solver that uses a non-recursive backtracking (depth-first search) algorithm.
+
 use std::collections::HashSet;
 use std::convert::{From, TryInto};
 use std::fmt;
@@ -23,17 +27,68 @@ fn is_set_legal<T: Iterator<Item = u32>>(values: T) -> bool {
     true
 }
 
+/// A container to hold a Sudoku grid.
+///
+///
+/// Although any ordering scheme (row-major vs. column-major) can be used to create the `Grid`, the
+/// [Display](std::fmt::Display) trait is implemented assuming row-major order.
+///
+/// ## Typical usage
+///
+/// 1. Ingest an unsolved puzzle into a `Grid` from a [&str] or some collection of [u32] using one
+/// of the available functions. Depending on the input type, 0 or '0' should be used to represent
+/// unfilled cells.
+/// 2. Use [`solve`](Grid::solve) to find a solution.
+/// 3. Display the result using the [`Display`](std::fmt::Display) trait or access the values using
+///    the [`as_slice`](Grid::as_slice) function.
+///
+/// ## Examples
+///
+/// Ingesting from a [&str], taking advantage of the [`FromStr`] trait implementation:
+///
+/// ```
+/// let puzzle: Grid = " \
+///     003020600 \
+///     900305001 \
+///     001806400 \
+///     008102900 \
+///     700000008 \
+///     006708200 \
+///     002609500 \
+///     800203009 \
+///     005010300"
+///     .parse()
+///     .unwrap();
+/// ```
+///
+/// Ingesting from a `&[u32; 81]` array using [`from_array`](Grid::from_array):
+///
+/// ```
+/// let puzzle: Grid = Grid::from_array(&[
+///     0, 0, 3, 0, 2, 0, 6, 0, 0, 9, 0, 0, 3, 0, 5, 0, 0, 1, 0, 0, 1, 8, 0, 6, 4, 0, 0, 0, 0,
+///     8, 1, 0, 2, 9, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 6, 7, 0, 8, 2, 0, 0, 0, 0, 2, 6,
+///     0, 9, 5, 0, 0, 8, 0, 0, 2, 0, 3, 0, 0, 9, 0, 0, 5, 0, 1, 0, 3, 0, 0,
+/// ]);
+/// ```
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Grid {
-    // Grid cells in row1, row2, etc. order
+    /// Array storing cells in `Grid`. Assumed to be in row-major order for the purposes of
+    /// pretty-printing the `Grid` and documenting the code.
     board: [u32; 81],
 }
 
 impl Grid {
+    /// Creates a new `Grid` from an array of [u32] values.
+    ///
+    /// 0 should be used to represent unfilled cells.
     pub const fn from_array(input: &[u32; 81]) -> Self {
         Self { board: *input }
     }
 
+    /// Returns a solution to the given `Grid`, if one exists.
+    ///
+    /// `solve()` copies out the solution into a new `Grid` object. It returns the first solution
+    /// found, even if multiple solutinos may exist. If no solution exists, it returns [None].
     pub fn solve(&self) -> Option<Self> {
         const ALL_SUDOKU_DIGITS: [u32; 9] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -58,8 +113,10 @@ impl Grid {
         None
     }
 
-    pub fn as_slice(&self) -> &[u32; 81] {
-        return &self.board;
+    /// Returns a slice over the elements in the `Grid`. The elements are returned in the same
+    /// ordering scheme (row-major vs. column-major) used to create the `Grid`.
+    pub const fn as_slice(&self) -> &[u32; 81] {
+        &self.board
     }
 
     fn is_solved(&self) -> bool {
@@ -67,7 +124,7 @@ impl Grid {
     }
 
     fn is_legal(&self) -> bool {
-        // Indices of the top left corners of each box of nine squares in a sudoku puzzle
+        /// Indices of the top left corners of each box of nine squares in a sudoku puzzle
         const NINTHS_IDXS: [usize; 9] = [0, 3, 6, 27, 30, 33, 54, 57, 60];
 
         for i in 0..9 {
@@ -97,6 +154,7 @@ impl Grid {
     }
 }
 
+/// Pretty-print a `Grid` assuming row-major order
 impl fmt::Display for Grid {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for i in (0..9).map(|i| 9 * i) {
@@ -141,6 +199,10 @@ impl From<Vec<u32>> for Grid {
 impl FromStr for Grid {
     type Err = String;
 
+    /// Parses a `Grid` from a string using the first 81 valid (0–10) digits in the string. Any
+    /// invalid digits are ignored. "0" should be used to represent unfilled cells.
+    ///
+    /// Returns [Err](Self::Err) if not enough digits were provided.
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         let board = input
             .chars()
